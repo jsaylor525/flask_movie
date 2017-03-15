3574a7c814653e38a3496236c27b82a0ef4704cf
Jonathan Saylor <jsaylor@bbn.com>
2017-03-15 10:45:29 -0500

    Finish tutorial 6
    * Dynamic profile page created.

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 Flask tutorial (Youtube)
 
 System Requirements:
-* Python 3.5.x [link](https://www.python.org/downloads/release/python-353/)
+* Python 3.5.x [link](https://www.python.org/downloads/release/python-353/) or greater
 
 run setup_env.bat on Windows machine to create a local envirnoment.
+
+Setup database
+> python
+> from app import db
+> db.create_all()
+> exit()
--- setup_env.bat
+++ setup_env.bat
@@ -1,11 +1,15 @@
-if not exist "env" mkdir "env"
-if not exist "source" mkdir "source"
-if not exist "source\css" mkdir "source\css"
-if not exist "source\js" mkdir "source\js"
-if not exist "source\images" mkdir "source\images"
-if not exist "templates" mkdir "templates"
+pip install virtualenv
 virtualenv env
 python -m pip install --upgrade pip
 pip install flask
 pip install Flask-SQLAlchemy
 pip install psycopg2
+pip install Flask-Security
+
+if not exist "source" mkdir "source"
+if not exist "source\css" mkdir "source\css"
+if not exist "source\js" mkdir "source\js"
+if not exist "source\images" mkdir "source\images"
+if not exist "templates" mkdir "templates"
+
+python setup_db.py
